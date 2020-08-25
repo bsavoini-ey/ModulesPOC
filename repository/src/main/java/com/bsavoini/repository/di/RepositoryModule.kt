ec4a72540c25fe40b8ec25cbd21adb0ce3168085
Bruno Savoini <bruno@freshworks.io>
2020-08-25 08:38:26 -0700

    Fix di

--- repository/src/main/java/com/bsavoini/repository/di/RepositoryModule.kt
+++ repository/src/main/java/com/bsavoini/repository/di/RepositoryModule.kt
@@ -1,10 +1,12 @@
 package com.bsavoini.repository.di
 
+import com.bsavoini.repository.FavoritesRepository
 import com.bsavoini.repository.FavoritesRepositoryImpl
+import com.bsavoini.repository.MediaRepository
 import com.bsavoini.repository.MediaRepositoryImpl
 import org.koin.dsl.module
 
 val repositoryModule = module {
-    factory { MediaRepositoryImpl() }
-    factory { FavoritesRepositoryImpl() }
+    single { MediaRepositoryImpl() as MediaRepository}
+    single { FavoritesRepositoryImpl() as FavoritesRepository}
 }
--- usecases/src/main/java/com/bsavoini/usecases/di/UseCasesModule.kt
+++ usecases/src/main/java/com/bsavoini/usecases/di/UseCasesModule.kt
@@ -3,9 +3,13 @@ package com.bsavoini.usecases.di
 import com.bsavoini.usecases.favorite.AddFavoriteUseCase
 import com.bsavoini.usecases.favorite.GetFavoriteUseCase
 import com.bsavoini.usecases.favorite.RemoveFavoriteUseCase
+import com.bsavoini.usecases.media.GetMoviesUseCase
+import com.bsavoini.usecases.media.GetTvShowsUseCase
 import org.koin.dsl.module
 
 val useCasesModule = module {
+    factory { GetMoviesUseCase(get()) }
+    factory { GetTvShowsUseCase(get()) }
     factory { AddFavoriteUseCase(get()) }
     factory { GetFavoriteUseCase(get()) }
     factory { RemoveFavoriteUseCase(get()) }

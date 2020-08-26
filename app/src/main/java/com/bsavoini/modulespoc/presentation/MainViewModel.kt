8a7cb626ff6b707be7425a32053515015a17202c
Bruno Savoini <bruno@freshworks.io>
2020-08-26 10:32:19 -0700

    Import coroutines and lifecycle

--- app/src/main/java/com/bsavoini/modulespoc/presentation/MainViewModel.kt
+++ app/src/main/java/com/bsavoini/modulespoc/presentation/MainViewModel.kt
@@ -1,13 +1,45 @@
 package com.bsavoini.modulespoc.presentation
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.bsavoini.interactor.FavoritesInteractor
 import com.bsavoini.interactor.MoviesInteractor
 import com.bsavoini.interactor.model.MovieModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class MainViewModel(
-    private val favoritesInteractor: FavoritesInteractor
-) {
+class MainViewModel(private val favoritesInteractor: FavoritesInteractor) : ViewModel() {
 
-    fun listFavorites() {}//favoritesInteractor.listFavorites()
+    fun listFavorites() {
+        launchBackgroundJob(
+            { favoritesInteractor.listFavorites() }
+        )
+    }
+
+    protected fun <T> launchBackgroundJob(
+        backgroundJob: suspend () -> T,
+        onSuccess: (T) -> Unit = {},
+        onError: (Throwable) -> Unit = {}
+    ): Job = viewModelScope.launch {
+        lunchBackgroundJobWithDispatching(
+            job = { backgroundJob() },
+            onSuccess = { result -> onSuccess(result) },
+            onError = { error -> onError(error) }
+        )
+    }
+
+    private suspend fun <T> lunchBackgroundJobWithDispatching(
+        job: suspend () -> T,
+        onSuccess: suspend (T) -> Unit,
+        onError: (Throwable) -> Unit
+    ) {
+        withContext(Dispatchers.IO) {
+            runCatching { job.invoke() }
+        }
+            .onSuccess { onSuccess(it) }
+            .onFailure { onError(it) }
+    }
 
 }
--- movies/src/main/java/com/bsavoini/movies/presentation/MoviesViewModel.kt
+++ movies/src/main/java/com/bsavoini/movies/presentation/MoviesViewModel.kt
@@ -1,18 +1,56 @@
 package com.bsavoini.movies.presentation
 
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.bsavoini.interactor.FavoriteMoviesInteractor
 import com.bsavoini.interactor.MoviesInteractor
 import com.bsavoini.interactor.model.MovieModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MoviesViewModel(
     private val moviesInteractor: MoviesInteractor,
     private val favoritesMoviesInteractor: FavoriteMoviesInteractor
-) {
+) : ViewModel() {
     fun listMovies() {
-      //  val list: List<MovieModel> = moviesInteractor.listMovies()
+        launchBackgroundJob(
+            {
+                val list: List<MovieModel> = moviesInteractor.listMovies()
+                Log.d("svn", "list: $list")
+            }
+        )
     }
 
     fun toggleFavorite(id: Int) {
-      //  favoritesMoviesInteractor.toggleFavorite(id)
+        launchBackgroundJob({
+            favoritesMoviesInteractor.toggleFavorite(id)
+        })
+    }
+
+    protected fun <T> launchBackgroundJob(
+        backgroundJob: suspend () -> T,
+        onSuccess: (T) -> Unit = {},
+        onError: (Throwable) -> Unit = {}
+    ): Job = viewModelScope.launch {
+        lunchBackgroundJobWithDispatching(
+            job = { backgroundJob() },
+            onSuccess = { result -> onSuccess(result) },
+            onError = { error -> onError(error) }
+        )
+    }
+
+    private suspend fun <T> lunchBackgroundJobWithDispatching(
+        job: suspend () -> T,
+        onSuccess: suspend (T) -> Unit,
+        onError: (Throwable) -> Unit
+    ) {
+        withContext(Dispatchers.IO) {
+            runCatching { job.invoke() }
+        }
+            .onSuccess { onSuccess(it) }
+            .onFailure { onError(it) }
     }
 }

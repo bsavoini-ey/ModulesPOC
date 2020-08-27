30459486e76c93ee9f4b134f4328870098394ad4
Bruno Savoini <bruno@freshworks.io>
2020-08-27 08:56:22 -0700

    Update FavoritesDao.kt

--- storage/src/main/java/com/bsavoini/storage/daos/FavoritesDao.kt
+++ storage/src/main/java/com/bsavoini/storage/daos/FavoritesDao.kt
@@ -10,15 +10,15 @@ import com.bsavoini.storage.entities.FavoriteEntity
 interface FavoritesDao {
 
     @Query("SELECT * FROM Favorite ORDER BY timestamp DESC")
-    fun selectAllBookmarks(): List<FavoriteEntity>
+    fun selectAllFavorites(): List<FavoriteEntity>
 
     @Query("SELECT * FROM Favorite where id = :id AND mediaType = :mediaType")
-    fun selectBookmark(id: Int, mediaType: String): List<FavoriteEntity>
+    fun selectFavorite(id: Int, mediaType: String): List<FavoriteEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertBookmark(favoriteEntity: FavoriteEntity)
+    fun insertFavorite(favoriteEntity: FavoriteEntity)
 
     @Query("DELETE FROM Favorite where id = :id AND mediaType = :mediaType")
-    fun removeBookmark(id: Int, mediaType: String): Int
+    fun removeFavorite(id: Int, mediaType: String): Int
 
 }
